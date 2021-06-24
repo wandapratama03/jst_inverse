7fc48749ad65e1dc6d536419352ed7f7d7755b15
Wanda Adi Pratama <p.wandaadi@gmail.com>
2021-06-25 06:26:25 +0700

    commit to main

---
+++ api/index.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var r = express.Router();
+
+// load pre-trained model
+const model = require('./sdk/model.js');
+
+// Bot Setting
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1832911184:AAE76Zkj9VNFzS0ABOmPvR9qjdP5GAk4xdA'
+const bot = new TelegramBot(token, {polling: true});
+
+
+// bots
+bot.onText(/\/start/, (msg) => { 
+    console.log(msg)
+    bot.sendMessage(
+        msg.chat.id,
+        `hello ${msg.chat.first_name}, welcome...\n
+        click /predict`
+    );   
+});
+
+bot.onText(/\/menu/, (msg) => { 
+    console.log(msg)
+    bot.sendMessage(
+        msg.chat.id,
+        `this is your main menu`
+    );   
+});
+
+state = 0
+bot.onText(/\/predict/, (msg) => { 
+    bot.sendMessage(
+        msg.chat.id,
+        `masukan nilai i|v contohnya 4|3`
+    );   
+    state = 1
+});
+
+bot.on('message', (msg) => {
+    if(state == 1){
+        s = msg.text.split("|");
+        i = s[0]
+        v = s[1]
+        model.predict(
+            [
+                parseFloat(s[0]), // string to float
+                parseFloat(s[1])
+            ]
+        ).then((jres)=>{
+            console.log(jres);
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai v yang diprediksi adalah ${jres[0]} volt`
+            );   
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai p yang diprediksi adalah ${jres[1]} watt`
+            );   
+        })
+    }else{
+        state = 0
+    }
+})
+
+// routers
+r.get('/prediction/:i/:r', function(req, res, next) {    
+    model.predict(
+        [
+            parseFloat(req.params.i), // string to float
+            parseFloat(req.params.r)
+        ]
+    ).then((jres)=>{
+        res.json(jres);
+    })
+});
+
+module.exports = r;
