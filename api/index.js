0a8b14b759f1ff6fd3c23f3ad8993dbb1343aed8
wandapratama03 <84654036+wandapratama03@users.noreply.github.com>
2021-06-25 10:34:46 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const model = require('./sdk/model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1753509171:AAFlg8grpwNAvyJpaYpYriNPC7AxwBkqFDw'
+const token = '1875119020:AAHGtM9gl6gdSqN7UOQwSFq207_CLw0Ic3o'
 const bot = new TelegramBot(token, {polling: true});
 
 
@@ -16,64 +16,61 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /predict`
+        click  /predict to know about x y and z`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
-    bot.sendMessage(
-        msg.chat.id,
-        `this is your main menu`
-    );   
-});
 
-state = 0
+
+state = 0;
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `masukan nilai x|y|z contohnya 4|3|5`
+        `input nilai x|y|z example 4|3|2`
     );   
-    state = 1
+    state = 1;
 });
 
 bot.on('message', (msg) => {
     if(state == 1){
         s = msg.text.split("|");
-        m1 = s[1]
-        m2 = s[2]
-        m3 = s[3]
+        x = s[0]
+        y = s[1]
+        z = s[2]
         model.predict(
             [
                 parseFloat(s[0]), // string to float
-                parseFloat(s[1])
+                parseFloat(s[1]),
+                parseFloat(s[2])
             ]
         ).then((jres)=>{
-            console.log(jres);
             bot.sendMessage(
                 msg.chat.id,
-                `nilai m1 yang diprediksi adalah ${jres[1]} degg`
+                `nilai x yang diprediksi adalah ${jres[0]} volt`
             );   
             bot.sendMessage(
                 msg.chat.id,
-                `nilai m2 yang diprediksi adalah ${jres[2]} degg`
+                `nilai y yang diprediksi adalah ${jres[1]} watt`
             );   
-             bot.sendMessage(
+            bot.sendMessage(
                 msg.chat.id,
-                `nilai m3 yang diprediksi adalah ${jres[3]} degg`
-            );   
-        })
+                `nilai z yang diprediksi adalah ${jres[2]} watt
+         );   
+})
     }else{
         state = 0
     }
 })
 
+
+
 // routers
-r.get('/prediction/:i/:r', function(req, res, next) {    
+r.get('/prediction/:x/:y/:z', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
+            parseFloat(req.params.x), // string to float
+            parseFloat(req.params.y),
+            parseFloat(req.params.z)
         ]
     ).then((jres)=>{
         res.json(jres);
